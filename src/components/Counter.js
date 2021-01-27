c9b4eaf0c14a9ce2604d4acae4b21578346a5fdb
Melissa <wolfaholic@users.noreply.github.com>
2021-01-27 06:41:38 -0800

    Update 1 - State Set / Counters Set
    Imported State and Set up the count, setcount

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,19 +46,19 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
-  /* STEP 1 */
+  const [count, setCount] = useState(0);
 
   const increment = () => {
-    /* STEP 4 */
+    setCount(count + 1);
   };
   const decrement = () => {
-    /* STEP 5 */
+      setCount(count - 1);
   };
   const reset = () => {
-    /* STEP 6 */
+    setCount(0)
   };
 
   const style = {
